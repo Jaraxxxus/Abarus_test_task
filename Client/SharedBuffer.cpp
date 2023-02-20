75c5d6421f4452140de7264402f1fd845ec76f90
Jaraxxxus <72243384+Jaraxxxus@users.noreply.github.com>
2023-02-20 14:58:03 +0700

    Update SharedBuffer.cpp

--- Client/SharedBuffer.cpp
+++ Client/SharedBuffer.cpp
@@ -14,7 +14,7 @@ void SharedBuffer::set(std::string &str) {
 
 std::string SharedBuffer::get() {
     std::unique_lock<std::mutex> lock(mutex);
-    cv.wait(lock, [this]() { return length != 0; });
+    cv.wait(lock);
     std::string result = std::move(data);
     data = "";
     length = 0;
